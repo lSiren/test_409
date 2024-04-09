c9415040b015fe027b482c70405a15a9c2309962
lSiren <2302034163@qq.com>
2024-04-09 16:55:03 +0800

    这是个测试3

--- first/src/Main.java
+++ first/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello my world!");
         System.out.println("Hello your world!");
+        System.out.println("Hello their world!");
     }
 }
